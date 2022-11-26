3dddd06ebc562987443fa0afbfe937c6a70651f4
psevdo212 <104160364+psevdo212@users.noreply.github.com>
2022-11-26 17:34:34 +0300

    AppHeader готов

---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,87 @@
+import {
+  Logo,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./appHeader.module.css";
+
+const AppHeader = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.list}>
+        <div className="mt-4 mb-4 ml-4">
+          <Button
+            htmlType="button"
+            type="secondary"
+            size="medium"
+            style={{
+              paddingTop: 16,
+              paddingBottom: 16,
+              paddingLeft: 20,
+              paddingRight: 20,
+              marginRight: 8,
+            }}
+          >
+            <ul className={styles.item}>
+              <li className="mr-2">
+                <BurgerIcon type="primary" />
+              </li>
+              <li className="text text_type_main-default">Конструктор</li>
+            </ul>
+          </Button>
+          <Button
+            htmlType="button"
+            type="secondary"
+            size="medium"
+            style={{
+              paddingTop: 16,
+              paddingBottom: 16,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }}
+          >
+            <ul className={styles.item}>
+              <li className="mr-2">
+                <ListIcon type="secondary" />
+              </li>
+              <li className="text text_type_main-default text_color_inactive">
+                Лента заказов
+              </li>
+            </ul>
+          </Button>
+        </div>
+        <div className="mt-5 mb-6" style={{ paddingRight: 88 }}>
+          <Logo />
+        </div>
+        <div className="mt-4 mb-4" style={{ paddingLeft: 87 }}>
+          <Button
+            htmlType="button"
+            type="secondary"
+            size="medium"
+            style={{
+              paddingTop: 16,
+              paddingBottom: 16,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }}
+          >
+            <ul className={styles.item}>
+              <li className="mr-2">
+                <ProfileIcon type="secondary" />
+              </li>
+              <li className="text text_type_main-default text_color_inactive">
+                Личный кабинет
+              </li>
+            </ul>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default AppHeader;
